7c6b45c26a2794592107237a82447c2fc24d6e5f
Ben Blain <mail@servc.eu>
2017-12-05 23:40:42 +0000

    Remove completion data on Linux

--- template/gui/MyCompletionData.cs
+++ template/gui/MyCompletionData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if !Linux
+using System;
 using ICSharpCode.AvalonEdit.CodeCompletion;
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Editing;
@@ -39,3 +40,4 @@ namespace gui
         }
     }
 }
+#endif
