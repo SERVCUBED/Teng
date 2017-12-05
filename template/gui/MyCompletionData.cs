c0302755aef05707f39b16071d3abc254e74c5bb
Ben Blain <mail@servc.eu>
2017-12-05 22:36:29 +0000

    Cleanup unused directives

--- template/gui/MyCompletionData.cs
+++ template/gui/MyCompletionData.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ICSharpCode.AvalonEdit.CodeCompletion;
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Editing;
